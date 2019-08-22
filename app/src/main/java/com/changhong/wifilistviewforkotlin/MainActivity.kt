c0915b8abf96701c0895df09790f625e7404af6d
liuyj <353381808@qq.com>
2019-08-22 14:44:56 +0800

    1. 添加打开wifi后不自动搜索的问题 2. ui界面上能够显示已连接、已保存

--- app/src/main/java/com/changhong/wifilistviewforkotlin/MainActivity.kt
+++ app/src/main/java/com/changhong/wifilistviewforkotlin/MainActivity.kt
@@ -17,6 +17,7 @@ import android.widget.Switch
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import android.location.LocationManager
+import android.net.wifi.WifiConfiguration
 import android.os.Build
 import android.support.annotation.RequiresApi
 import java.util.jar.Manifest
@@ -33,9 +34,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val locManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         wifiManager = getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
-
 
         this.requestPermissions(arrayOf("android.permission.ACCESS_COARSE_LOCATION"), 1)
         this.requestPermissions(arrayOf("android.permission.ACCESS_FINE_LOCATION"), 1)
@@ -67,15 +66,46 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(wifiBroadcastReceiver, intentFilter)
     }
 
+    private fun isExistedConfiguration(scanResult: ScanResult): Boolean {
+        val configurationList = wifiManager.configuredNetworks
+        for (wifiConfiguration in configurationList) {
+            if (wifiConfiguration.SSID.equals("\"" + scanResult.SSID + "\"")) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    private fun packageWifiItem(scanResult: ScanResult, info: String = ""): Map<String, String> {
+        return mapOf(
+                "info" to info,
+                "frequency" to "[${scanResult.frequency.toString()}]",
+                "ssid" to scanResult.SSID,
+                "bssid" to scanResult.BSSID,
+                "rssi" to WifiManager.calculateSignalLevel(scanResult.level, 10).toString())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun updateDataAndView() {
+        var isFlag = false
         dataList.clear()
         println("wifiManager.scanResults.size: " + wifiManager.scanResults.size)
         for (scanResult in wifiManager.scanResults) {
-            dataList.add(
-                mapOf(
-                    "ssid" to scanResult.SSID,
-                    "bssid" to scanResult.BSSID,
-                    "rssi" to WifiManager.calculateSignalLevel(scanResult.level, 10).toString()))
+            if (isExistedConfiguration(scanResult)) {
+
+                if ((wifiManager.connectionInfo.ssid == "\"" + scanResult.SSID + "\"")
+                        and (wifiManager.connectionInfo.bssid == scanResult.BSSID)) {
+                    dataList.add(0, packageWifiItem(scanResult, "已连接"))
+                    isFlag = true
+
+                } else {
+                    dataList.add(if(isFlag) 1 else 0, packageWifiItem(scanResult, "已保存"))
+
+                }
+            } else{
+                dataList.add(packageWifiItem(scanResult))
+            }
         }
         wifiBaseAdapter.notifyDataSetChanged()
 
@@ -88,9 +118,11 @@ class MainActivity : AppCompatActivity() {
             var layoutInflater = LayoutInflater.from(parent?.context)
             var view = layoutInflater.inflate(R.layout.layout_listview_wifi_item, parent, false)
 
+            view.findViewById<TextView>(R.id.textview_frequency).text = dataList.get(position).get("frequency")
             view.findViewById<TextView>(R.id.textview_ssid).text = dataList.get(position).get("ssid")
             view.findViewById<TextView>(R.id.textview_bssid).text = dataList.get(position).get("bssid")
             view.findViewById<TextView>(R.id.rssi).text = dataList.get(position).get("rssi")
+            view.findViewById<TextView>(R.id.textview_info).text = dataList.get(position).get("info")
 
             return view
         }
@@ -123,6 +155,7 @@ class MainActivity : AppCompatActivity() {
 
                         3 -> {
                             switch_wifi.isChecked = true
+                            wifiManager.startScan()
                             updateDataAndView()
                         }
                     }
